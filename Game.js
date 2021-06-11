b6e7c23c3cdf693a7fe6ed83034ad58bacca91d6
Megan McBride <megan.mcbride@colorado.edu>
2021-06-11 13:31:46 -0600

    Make it through the winning conditionals to the function to fall the win

--- Game.js
+++ Game.js
@@ -15,12 +15,8 @@ class Game {
   }
 
   updateGameBoard(sectionId, playerName) {
+    console.log(gameSectionStatus);
     gameSectionStatus[sectionId] = playerName;
-    // for (var i = 0; i < winningConditions.length; i++) {
-    //     if (winningConditions[i][sectionId] === false) {
-    //     winningConditions[i][sectionId] = playerName;
-    //   }
-    // }
     this.evaluateRows(playerName);
   }
 
@@ -35,26 +31,9 @@ class Game {
         this.evaluateColumns(playerName)
       }
     }
-    // var rowOne = Object.values(winningConditions[3]);
-    //   if (rowOne.every(this.isPigWinner) || rowOne.every(this.isTigerWinner)) {
-    //     console.log('yes')
-    //     //add return and then move to else if statement for row 2
-    //   }
-    //
-    // var rowTwo = Object.values(winningConditions[4]);
-    // var rowThree = Object.values(winningConditions[5]);
-    // }
-
-
-  // isPigWinner(currentValue) {
-  //   return (currentValue === 'Pig')
-  // }
-  //
-
-
 
   evaluateColumns(playerName) {
-    if (gameSectionStatus.item1 === playerName && gameSectionStatus.item2 === playerName && gameSectionStatus.item3 === playerName) {
+      if (gameSectionStatus.item1 === playerName && gameSectionStatus.item2 === playerName && gameSectionStatus.item3 === playerName) {
       this.callAWin(playerName);
     } else if (gameSectionStatus.item4 === playerName && gameSectionStatus.item5 === playerName && gameSectionStatus.item6 === playerName) {
       this.callAWin(playerName);
@@ -64,6 +43,7 @@ class Game {
       this.evaluateDiagonals(playerName)
     }
   }
+
   evaluateDiagonals(playerName) {
     if (gameSectionStatus.item1 === playerName && gameSectionStatus.item5 === playerName && gameSectionStatus.item9 === playerName) {
       this.callAWin(playerName);
@@ -73,35 +53,50 @@ class Game {
       this.callADraw()
     }
   }
-  //
+
   callADraw(currentBoard) {
     var modifiedGame = Object.values(gameSectionStatus);
     var isGameFull = modifiedGame.every(this.isFull);
     if (!isGameFull) {
-      console.log('Uh oh');
-    };
-    // assess if all values
-    //Update localStorage with whose turn it is
+      this.changeTurns();
+    } else {
+      this.turns++;
+      //localStorage for whose turn it is
+      //and invoke the method to reset the Game
+    }
   }
 
   isFull(currentValue) {
       return (currentValue === 'Pig' || currentValue === 'Tiger');
     }
-  //
-  // callAWin(winningPlayer) {
-  //   // Change <h1> to show winning player
-  //   // Add one win to the winning player's count. (winningPlayer.wins++;)
-  //   // invoke the saveWinsToStorage for the winning Player
-  //   //Update localStorage with whose turn it is
-  //   //invoke setNewGame();
-  // }
+
+  changeTurns() {
+    this.turn++;
+    //invoke the function to update to the DOM for the next player - both in the
+    //heading of whose turn as well as with the icon that will be dropped
+  }
+
+  callAWin(winningPlayer) {
+    console.log(winningPlayer);
+    console.log('game player name', this.pig)
+    // winningPlayer.wins++;
+    // winningPlayer.saveWinsToStorage();
+    this.setNewGame();
+    // invoke DOM to update <h1> with winningPlayer's name
+    // Change <h1> to show winning player
+    // Add one win to the winning player's count. (winningPlayer.wins++;)
+    // invoke the saveWinsToStorage for the winning Player
+    //Update localStorage with whose turn it is
+    //invoke setNewGame();
+  }
 
   //
-  // setNewGame() {
-  //   // removing all added items,
-  //   // showing the updated # of wins for the players - retrievedFromStorage
-  //   // Displaying whose turn is next - retrievedFromStorage
-  // }
+  setNewGame() {
+    console.log('Reset!')
+    // removing all added items,
+    // showing the updated # of wins for the players - retrievedFromStorage
+    // Displaying whose turn is next - retrievedFromStorage
+  }
 }
 
   // where to trackTurns
