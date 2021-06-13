f3fdc51d5ecbe66a8990baa26479ea5ed013b767
Megan McBride <megan.mcbride@colorado.edu>
2021-06-13 17:38:09 -0600

    Create the functionality to allow players to enter their name and then have it display

--- main.js
+++ main.js
@@ -1,9 +1,15 @@
 //Query Selectors
   var gameBoard = document.getElementById('board');
   var playerOnesWins = document.getElementById('player-one-wins');
+  var playerOneForm = document.getElementById('player-one-form');
   var playerOneName = document.getElementById('player-one-name');
+  var playerOneSubmitBtn = document.getElementById('player-one-submit');
+  var playerOneNameDisplay = document.getElementById('one-name-display');
+  var playerTwoForm = document.getElementById('player-two-form');
   var playerTwosWins = document.getElementById('player-two-wins');
   var playerTwoName = document.getElementById('player-two-name');
+  var playerTwoSubmitBtn = document.getElementById('player-two-submit');
+  var playerTwoNameDisplay = document.getElementById('two-name-display');
   var whoseTurn = document.getElementById('whose-turn');
   var centerPanel = document.getElementById('center-panel')
   var clearHistoryBtn =document.getElementById('clear-btn');
@@ -13,6 +19,10 @@ var theGame;
 
 //Event Listeners
 window.addEventListener('load', loadGame);
+playerOneSubmitBtn.addEventListener('click', function(event) {
+  displayPlayerOneName(event)});
+playerTwoSubmitBtn.addEventListener('click', function(event) {
+  displayPlayerTwoName(event)});
 gameBoard.addEventListener('click', function(event) {
   populateSection(event)});
 centerPanel.addEventListener('click', function(event) {
@@ -60,6 +70,18 @@ function displayPlayerStoredWins(player, winDisplay) {
 
 function displayTurn() {
   whoseTurn.innerHTML = `It's ${theGame.trackTurns().token}'s Turn!`;
+}
+
+function displayPlayerOneName(event) {
+  event.preventDefault(event);
+  playerOneForm.classList.add('hidden');
+  playerOneNameDisplay.innerText = playerOneName.value;
+}
+
+function displayPlayerTwoName(event) {
+  event.preventDefault(event);
+  playerTwoForm.classList.add('hidden');
+  playerTwoNameDisplay.innerText = playerTwoName.value;
 }
 
 function populateSection(event) {
